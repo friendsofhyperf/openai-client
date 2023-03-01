4057e59da4f37ae934bc7623e44e5623e8570071
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-03-01 08:32:57 +0800

    Adds test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of openai-client.
+ *
+ * @link     https://github.com/friendsofhyperf/openai-client
+ * @document https://github.com/friendsofhyperf/openai-client/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+test('example', function () {
+    $this->assertTrue(true);
+});
