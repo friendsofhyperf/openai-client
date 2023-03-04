47b4d107d93ac60074ff36d2fc43dedb7aeed4bb
Deeka Wong <huangdijia@gmail.com>
2023-03-05 06:48:23 +0800

    Bump `openai-php/client` to `0.3.4` (#2)
    * Revert ClientFactory
    
    * Format
    
    * Bump openai-client to 0.3.4

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,17 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of openai-client.
+ *
+ * @link     https://github.com/friendsofhyperf/openai-client
+ * @document https://github.com/friendsofhyperf/openai-client/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
 }
